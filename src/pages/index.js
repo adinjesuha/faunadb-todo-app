9fce4ed01e93c6c412bf9281423daaa22e6a231d
Adin Castro <acastro@agrobiotek.com>
2020-05-19 10:40:17 -0600

    add netlify-identity-widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Container, Heading, Button, Flex  } from 'theme-ui'
+import React, { useEffect } from 'react';
+import { Container, Heading, Button, Flex  } from 'theme-ui';
+import netlifyIdentity from 'netlify-identity-widget';
 
-export default props => (
-  <Container>
-  <Flex sx={{flexDirection: "column", padding: 3}}>
-    <Heading as="h1">Hello world!!</Heading>
-     <Button 
-      sx={{ marginTop: 2}}
-      onClick={() => alert('click')}
-    >Log in</Button>
-  </Flex>
-  </Container>
-)
+export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  })
+  return (
+    <Container>
+      <Flex sx={{flexDirection: "column", padding: 3}}>
+        <Heading as="h1">Hello world!!</Heading>
+        <Button 
+          sx={{ marginTop: 2}}
+          onClick={() => {
+            netlifyIdentity.open()
+          }}
+        >Log in</Button>
+      </Flex>
+    </Container>
+  )
+}
