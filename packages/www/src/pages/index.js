6d09ee043f78382dcb55bbcbc6bddf9199f9e4c5
Adin Castro <acastro@agrobiotek.com>
2020-05-18 23:19:47 -0600

    add theme-ui

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Container, Heading, Button, Flex  } from 'theme-ui'
 
 export default props => (
-  <div>
-    <h1>Hello world!!</h1>
-  </div>
+  <Container>
+  <Flex sx={{flexDirection: "column", padding: 3}}>
+    <Heading as="h1">Hello world!!</Heading>
+     <Button 
+      sx={{ marginTop: 2}}
+      onClick={() => console.log('click')}
+    >Log in</Button>
+  </Flex>
+  </Container>
 )
